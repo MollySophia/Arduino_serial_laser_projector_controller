476fc1b1f1a8abac9b67cf6a4317b1facac4cabf
Molly Sophia <mollysophia379@gmail.com>
2021-06-16 21:33:58 +0800

    Initial UI support

--- src/Projector.cpp
+++ src/Projector.cpp
@@ -12,16 +12,43 @@ int pinB = 12;
 int pinSW = 13;
 
 #include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
-#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-SSD1306 display(0x3c, 4, 5);
-int menuID = 0;
+#include "images.h"
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3c
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int menuID = 0, listID = 0, listStr = 0;
 int keyState = 0;
-int rotatorState = 0;
+int curCLK, lastCLK;
+int rotatorDiff = 0, rotatorDiffLast = 0;
+int powerState = 0, keystoneVertical = 0, keystoneHorizontal = 0;
+unsigned long ms = 0;
+
 
 IRAM_ATTR void keyISR() {
+  ms = millis();
   while(digitalRead(pinSW) == LOW);
-  keyState = 1;
+  if(millis() - ms >= 400) keyState = 2;
+  else keyState = 1;
+}
+
+IRAM_ATTR void rotatorISR() {
+  curCLK = digitalRead(pinA);
+
+  if(curCLK != lastCLK && curCLK == 0) {
+    if(digitalRead(pinB) != curCLK) rotatorDiff++;
+    else rotatorDiff--;
+  }
+
+  lastCLK = curCLK;
+  //delay(1);
 }
 
 void powerOnOff();
@@ -35,6 +62,29 @@ void factoryModeExit(bool save);
 void saveUserConfig();
 void restoreDefaultConfig();
 
+void drawCenterString(const String &buf, int x, int y);
+void drawProgressBar(int x, int y, int w, int h, int percentage);
+void drawTitle(const char *string);
+void drawListEntry(String string, int num, int selected);
+
+#define MENU_MAX_NUM 5
+#define FLIP_MAX_NUM 3
+String menuList[6] = {
+  "Flip",
+  "Keystone",
+  "Brightness",
+  "Contrast",
+  "Save",
+  "Restore"
+};
+
+String flipList[4] = {
+  "Flip both",
+  "Y-Flip",
+  "X-Flip",
+  "Normal"
+};
+
 void setup() {
   WiFi.begin (ssid, password);
 
@@ -43,29 +93,33 @@ void setup() {
     delay(10);
   }
 
-  display.init();
-  display.flipScreenVertically();
-  display.setContrast(255);
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  display.clearDisplay();
+  display.drawBitmap(0, 0, bootsplash, 128, 64, SSD1306_WHITE);
+  display.display();
+
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  //display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setTextColor(SSD1306_INVERSE);
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
   ArduinoOTA.begin();
   ArduinoOTA.onStart([]() {
-    display.clear();
-    display.setFont(ArialMT_Plain_10);
-    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "OTA Update");
+    display.clearDisplay();
+    drawCenterString("OTA Update", 64, 32 - 20);
     display.display();
   });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    display.drawProgressBar(4, 32, 120, 8, progress / (total / 100) );
+    drawProgressBar(4, 32, 120, 8, progress / (total / 100));
     display.display();
   });
 
   ArduinoOTA.onEnd([]() {
-    display.clear();
-    display.setFont(ArialMT_Plain_10);
-    display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-    display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Restart");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.printf("Update completed.\nRebooting...\n");
     display.display();
   });
 
@@ -75,48 +129,202 @@ void setup() {
   pinMode(pinSW, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(pinSW), keyISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(pinA), rotatorISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pinB), rotatorISR, CHANGE);
+  lastCLK = digitalRead(pinA);
+
+
+  delay(3000);
+}
+
+void actionHandle() {
+  while(keyState == 0 && (rotatorDiff - rotatorDiffLast == 0)) {
+    ArduinoOTA.handle();
+    delay(100);
+  }
+
+  int tmp_val = 0;
+  char tmp_string[10];
+
+  switch (menuID) {
+    case 0:
+      if(keyState == 1) {
+        powerOnOff();
+        display.clearDisplay();
+        if(powerState == 0) {
+          drawCenterString("Starting", 64, 32 - 20);
+          powerState = 1;
+        } else {
+          drawCenterString("Stopping", 64, 32 - 20);
+          powerState = 0;
+        }
+        keyState = 0;
+        for(int i = 0; i < 10; i++) {
+          drawProgressBar(4, 32, 120, 8, i * 10 );
+          display.display();
+          delay(400);
+        }
+      } else if(keyState == 2) {
+        menuID = 1;
+        keyState = 0;
+        listID = 0;
+        listStr = 0;
+      }
+      break;
+
+    case 1: //menu
+      if(keyState == 1) {
+        switch (listID) {
+          case 0: 
+            menuID = 2; //flip
+            break;
+          case 1:
+            menuID = 3; //keystone
+            break;
+          case 2:
+            menuID = 4; //brightness
+            break;
+          case 3:
+            menuID = 5; //contrast
+            break;
+          case 4:
+            saveUserConfig();
+            delay(500);
+            break;
+          case 5:
+            restoreDefaultConfig();
+            delay(500);
+            break;
+        }
+        listStr = 0;
+        listID = 0;
+      } else if(keyState == 2) {
+        menuID = 0;
+        keyState = 0;
+      }
+      if(rotatorDiff != rotatorDiffLast) {
+        if(rotatorDiff > rotatorDiffLast)
+          listID++;
+        else listID--;
+
+        if(listID < 0) listID = 0;
+        if(listID > MENU_MAX_NUM) listID = MENU_MAX_NUM;
+        if(listID - listStr >= 3) listStr++;
+        if(listID - listStr < 0) listStr = listID;
+      }
+      break;
+
+    case 2://flip settings
+      if(keyState == 1) {
+        flipScreen(listID);
+        delay(500);
+        listStr = 0;
+        listID = 0;
+        menuID = 1;
+      }
+      if(rotatorDiff != rotatorDiffLast) {
+        if(rotatorDiff > rotatorDiffLast)
+          listID++;
+        else listID--;
+
+        if(listID < 0) listID = 0;
+        if(listID > FLIP_MAX_NUM) listID = FLIP_MAX_NUM;
+        if(listID - listStr >= 3) listStr++;
+        if(listID - listStr < 0) listStr = listID;
+      }
+      break;
+
+    case 3://keystone
+      if(keyState == 1) {
+        keyState = 0;
+        display.clearDisplay();
+        while(keyState == 0) {
+          if(rotatorDiff - rotatorDiffLast > 0) tmp_val++;
+          if(rotatorDiff - rotatorDiffLast < 0) tmp_val--;
+          rotatorDiffLast = rotatorDiff;
+          if(tmp_val > 30) tmp_val = 30;
+          if(listID == 0 && tmp_val < -15) tmp_val = -15;
+          if(listID == 0 && tmp_val < -30) tmp_val = -30;
+          sprintf(tmp_string, "- %i +", tmp_val);
+          display.clearDisplay();
+          drawCenterString(tmp_string, 64, 32);
+          display.display();
+          if(listID == 0) keystoneCorrectionVertical(tmp_val);
+          else keystoneCorrectionHorizontal(tmp_val);
+          delay(200);
+        }
+        listID = 0;
+        listStr = 0;
+        menuID = 1;
+      }
+      if(rotatorDiff != rotatorDiffLast) {
+        if(rotatorDiff > rotatorDiffLast)
+          listID++;
+        else listID--;
+
+        if(listID < 0) listID = 0;
+        if(listID > 1) listID = 1;
+        if(listID - listStr >= 3) listStr++;
+        if(listID - listStr < 0) listStr = listID;
+      }
+
+    default:
+      break;
+  }
+
+  rotatorDiffLast = rotatorDiff;
+  keyState = 0;
+}
+
+void uiHandle() {
+  switch(menuID) {
+    case 0:
+      display.clearDisplay();
+      if(powerState == 0)
+        display.drawBitmap(0, 0, poweroff, 128, 64, SSD1306_WHITE);
+      else
+        display.drawBitmap(0, 0, poweron, 128, 64, SSD1306_WHITE);
+      display.display();
+      break;
+
+    case 1: //menu
+      display.clearDisplay();
+      drawTitle("Menu");
+
+      for(int i = 0; i < 3; i ++) {
+        drawListEntry(menuList[listStr + i], i+1, listStr+i == listID ? 1: 0);
+      }
+
+      display.display();
+      break;
+    
+    case 2: //flip
+      display.clearDisplay();
+      drawTitle("Flip");
+
+      for(int i = 0; i < 3; i ++) {
+        drawListEntry(flipList[listStr + i], i+1, listStr+i == listID ? 1: 0);
+      }
+
+      display.display();
+      break;
+
+    case 3: //keystone
+      display.clearDisplay();
+      drawTitle("Keystone");
+      drawListEntry("Vertical", 1, listID == 0 ? 1 : 0);
+      drawListEntry("Horizontal", 2, listID == 1 ? 1 : 0);
+      display.display();
+      break;
+
+    default: 
+      break;  
+  }
 }
 
 void loop() {
-  switch(menuID) {
-    case 0:
-      display.clear();
-      display.setFont(ArialMT_Plain_10);
-      display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-      display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Press to power on.");
-      display.display();
-      while(keyState == 0) delay(100);
-      display.clear();
-      display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2 - 10, "Starting...");
-      display.display();
-      menuID = 1;
-      powerOnOff();
-      for(int i = 0; i < 10; i++) {
-        display.drawProgressBar(4, 32, 120, 8, i * 10 );
-        display.display();
-        delay(400);
-      }
-      keyState = 0;
-      break;
-    case 1:
-      display.clear();
-      display.setFont(ArialMT_Plain_10);
-      display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-      display.drawString(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2, "Press to power off.");
-      display.display();
-      while(keyState == 0) delay(100);
-      menuID = 0;
-      powerOnOff();
-      delay(5000);
-      keyState = 0;
-      break;
-    case 100: //ota
-      while(1) {
-        ArduinoOTA.handle();
-      }
-      break;
-    default: menuID = 1; break;  
-  }
+  uiHandle();
+  actionHandle();
 }
 
 void powerOnOff() {
@@ -192,4 +400,46 @@ void restoreDefaultConfig() {
   unsigned char tmp[8] = {0xff, 0x07, 0x77, 0x00, 0x00, 0x00, 0x00, 0x7e};
   Serial.write(tmp, 8);
   delay(100);
+}
+
+void drawProgressBar(int x, int y, int w, int h, int percentage) {
+  display.drawRoundRect(x, y, w, h, 5, SSD1306_WHITE);
+  display.fillRoundRect(x + 2, y + 2, (int)((w - 4) / 100.0 * percentage), h - 4, 3, SSD1306_WHITE);
+}
+
+void drawCenterString(const String &buf, int x, int y) {
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.getTextBounds(buf, x, y, &x1, &y1, &w, &h); //calc width of new string
+    display.setCursor(x - w / 2, y);
+    display.print(buf);
+}
+
+void drawTitle(const char *string) {
+  drawCenterString(string, 64, 0);
+  display.drawFastHLine(0, 17, 128, SSD1306_WHITE);
+  display.drawFastHLine(0, 16, 128, SSD1306_WHITE);
+}
+
+void drawListEntry(String string, int num, int selected) {
+  switch(num) {
+    case 1:
+      if(selected)
+        display.fillRect(0, 19, 128, 15, SSD1306_WHITE);
+      display.setCursor(2, 19);
+      display.print(string);
+      break;
+    case 2:
+      if(selected)
+        display.fillRect(0, 34, 128, 15, SSD1306_WHITE);
+      display.setCursor(2, 35);
+      display.print(string);
+      break;
+    case 3:
+      if(selected)
+        display.fillRect(0, 49, 128, 15, SSD1306_WHITE);
+      display.setCursor(2, 50);
+      display.print(string);
+      break;
+  }
 }
